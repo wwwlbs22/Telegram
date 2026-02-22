b17b30804a96394a8054d2c68d175709fd7e78a3
⚠️ ✘ 𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏 ✘ ⚠️ 🚩 <154514127+wwwlbs22@users.noreply.github.com>
2026-02-22 08:16:35 +0530

    Update bot token and owner ID in config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ load_dotenv()
 
 API_ID = "24509589"
 API_HASH = "717cf21d94c4934bcbe1eaa1ad86ae75"
-BOT_TOKEN = "8568457723:AAFeCfdNRIi6wNYtlhfpO91WFNlG40fHEkc"
+BOT_TOKEN = "8368861592:AAFC_c6f8DU4_bo3PqDc0c43s3MwGJw2lkM"
 MONGO_URL = "mongodb+srv://vclub:vclub@vclub.hauilrr.mongodb.net/?retryWrites=true&w=majority"
-OWNER_ID = "8322551557"
+OWNER_ID = "7072987404"
 SUPPORT_GRP = "HEROKUFREECC"
 UPDATE_CHNL = "ALLTYPECC"
 OWNER_USERNAME = "Imeshu"
 
 # Additional settings
-OWNER = [8322551557]
+OWNER = [7072987404]
